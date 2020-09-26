2eec64489adcfd0c16a67d9b6bdfb6fcad66a860
Andi Meier <andi@daemeier-clan.ch>
2020-09-26 15:18:59 +0200

    Play audio upon completion
    Create a link in ./android/app/src/main/assets/ to work around bug:
    $ cd ./android/app/src/main/assets/
    $ ln -s ./public/ www

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx'
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -15,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    NativeAudio,
     StatusBar,
     ScreenOrientation,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonRouterOutlet, Platform } from '@ionic/angular';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import { Plugins } from '@capacitor/core';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 const { App } = Plugins;
@@ -15,19 +16,20 @@ export class HomePage {
 
   DURATION_IN_SECONDS = 120;
   MILLISECONDS_PER_SECOND = 1000;
+  OVI_ISCH_FERTIG_AUDIO_ASSET_PATH = 'assets/ovi_isch_fertig.aac';
   end_time: Date;
   seconds: number;
   progressbar_value: number;
   timer_subscription: any;
 
   constructor(
+    private nativeAudio: NativeAudio,
     private platform: Platform,
     private routerOutlet: IonRouterOutlet,
     private screenOrientation: ScreenOrientation
   ) {
     this.platform.backButton.subscribeWithPriority(-1, () => {
       if (!this.routerOutlet.canGoBack()) {
-        console.log('exiting...');
         App.exitApp();
       }
       else {
@@ -38,38 +40,50 @@ export class HomePage {
   }
 
   ionViewDidEnter() {
-    console.log('ionViewDidEnter...');
-    this.setupAndStartTimer();
+    this.setupAndStartTimerForCountdown();
   }
 
-  evalTimeDifference() {
+  private evalTimeDifference() {
     const now = new Date();
     const remaining_time_in_milliseconds = this.end_time.getTime() - now.getTime();
     console.log('remaining_time_in_milliseconds:', remaining_time_in_milliseconds)
     const is_time_elapsed = remaining_time_in_milliseconds <= 0;
     if (!is_time_elapsed) {
-      this.seconds = Math.floor(remaining_time_in_milliseconds / 1000);
+      this.seconds = 1 + Math.floor(remaining_time_in_milliseconds / 1000);
       this.progressbar_value = 1 - (remaining_time_in_milliseconds / (this.DURATION_IN_SECONDS * this.MILLISECONDS_PER_SECOND));
     }
     else {
       this.seconds = 0;
       this.progressbar_value = 1;
       this.stopTimer();
+      this.setupAndStartTimerForPlaySound();
     }
   }
 
-  private setupAndStartTimer() {
+  private setupAndStartTimerForCountdown() {
     const now = new Date();
-    console.log('now:', now);
     const duration_in_milliseconds = this.DURATION_IN_SECONDS * this.MILLISECONDS_PER_SECOND;
     this.end_time = new Date(now.getTime() + duration_in_milliseconds);
-    console.log('ts_end:', this.end_time);
-
     const source = timer(0, 100);
     this.timer_subscription = source.subscribe(() => this.evalTimeDifference());
   }
 
   private stopTimer() {
     this.timer_subscription.unsubscribe();
+  }
+
+  private setupAndStartTimerForPlaySound() {
+    const source = timer(0, 3000);
+    this.timer_subscription = source.subscribe(() => this.playSound());
+  }
+
+  private playSound() {
+    this.nativeAudio.preloadSimple('audio_1', this.OVI_ISCH_FERTIG_AUDIO_ASSET_PATH).then((value: any) => {
+      console.log('preload succeeded:', value);
+      this.nativeAudio.play('audio_1');
+    }, (reason: any) => {
+      console.log('preload failed:', reason);
+      this.nativeAudio.play('audio_1');
+    });
   }
 }
